bad3781da5217a2b74b7ce8d305346ea5a3cc3ae
annyhira414 <annyhira@gmail.com>
2022-05-14 02:40:00 +0600

    madlibs

--- 12-project for beginners/Madlibs/madlibs.py
+++ 12-project for beginners/Madlibs/madlibs.py
@@ -1,1 +1,29 @@
-print("start 12 project ")
+name = "Hira"
+print(f"My name is {name}")
+name1 = "anny"
+print("Hi i am %s" % name1)
+
+color = "green"
+color1 = "blue"
+color2 = "white"
+
+like = f"I like this 3 colors , which are {color },{color1},{ color2}"
+print(like)
+
+print(f"I like this 3 colors , which are {color },{color1}, {color2}")
+
+noun = input("write nouns : ")
+noun1 = input("write noun : ")
+verb = input("write verb : ")
+madlibs = f"I am {noun} I love {noun1} \
+I like to {verb} cake"
+
+print(madlibs)
+
+
+
+'''
+mini project name of project : mad-libs time 2.10 am  date : 5/14/2022
+'''
+
+
