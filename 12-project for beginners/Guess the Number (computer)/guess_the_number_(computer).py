d42fb9ec73cde682be8d601cd6e1b5ae65dc9e6a
annyhira414 <annyhira@gmail.com>
2022-05-14 18:25:53 +0600

    guess_the_number

---
+++ 12-project for beginners/Guess the Number (computer)/guess_the_number_(computer).py
@@ -0,0 +1,18 @@
+import random
+
+
+# print(random.randint(1 , 6))
+# normal way, but now we are using a function
+
+def guess(x):
+    my_random_number = random.randint(1, x)
+
+'''
+2nd steps - ones is done then the computer has a random number , we need to guess the random number .
+we need to use input and the computer will  tell us whether its too high oe too low ,
+or if i guess the number then computer say the number is correctly. 
+so I need to keep looping until I get the right answer
+'''
+'''
+date- 5/14/22 time 1.50 pm .
+'''
