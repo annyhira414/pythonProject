2f64d6b4fa5794f7292101ff4731e566d0ac1e49
annyhira414 <annyhira@gmail.com>
2022-05-16 16:51:15 +0600

    computer guess my number

--- 12-project for beginners/Guess the number form User/guess_number_from_user.py
+++ 12-project for beginners/Guess the number form User/guess_number_from_user.py
@@ -1,9 +1,23 @@
 import random
 
+'''
+computer guess my number .
+'''
+
 
 def computer_guess(x):
     low = 1
     high = x
     feedback = " "
     while feedback != 'c':
-        random_number = int(random.randint(low, high))
+        guess_number = int(random.randint(low, high))
+        feedback = input(f"its {guess_number} number is too high(H) , too low (l) or correct (c) : ").lower()
+        if feedback == 'h':
+            high = guess_number - 1
+        elif feedback == 'l':
+            low = guess_number - 1
+
+    print(f"good job computer {guess_number} it is my number ")
+
+
+computer_guess(10)
