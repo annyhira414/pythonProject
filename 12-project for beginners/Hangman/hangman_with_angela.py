12cc95c0b0252e5abf82e5249847604bb00ebe56
annyhira414 <annyhira@gmail.com>
2022-05-21 23:29:30 +0600

    hungman

--- 12-project for beginners/Hangman/hangman_with_angela.py
+++ 12-project for beginners/Hangman/hangman_with_angela.py
@@ -4,9 +4,13 @@ data = ["mouse", "apple", "rice"]
 chosen_word = random.choice(data)
 print(chosen_word)
 
+
 # s2
 guess = input("please guess the letters:  ").lower()
-
+display = []
+for _ in chosen_word:
+    display += "_"
+print(display)
 # if chosen_word in data:
 # if the later the user guessed is one of the letters in the chosen word
 
